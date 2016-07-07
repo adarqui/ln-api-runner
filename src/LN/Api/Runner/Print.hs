59bf7824e2eb6fb12c1f803b62a46e581eacebca
Andrew Darqui <andrew.darqui@gmail.com>
2016-07-06 21:50:02 -0400

    cleanup

--- src/LN/Api/Runner/Print.hs
+++ src/LN/Api/Runner/Print.hs
@@ -5,51 +5,26 @@
 {-# LANGUAGE OverloadedStrings     #-}
 {-# LANGUAGE RecordWildCards       #-}
 
-module LN.Api.Runner.Print where
+module LN.Api.Runner.Print  (
+  printFail,
+  printActualFailure,
+  printFatal,
+  printPass,
+  printInfo,
+  printSection,
+) where
 
 
 
-import           Control.Break              (break, loop)
-import           Control.Concurrent         (forkIO, threadDelay)
-import           Control.Exception
-import           Control.Monad              (void)
-import           Control.Monad
-import           Control.Monad.Except
-import           Control.Monad.IO.Class     (liftIO)
-import           Control.Monad.Trans.Either (EitherT, runEitherT)
-import qualified Control.Monad.Trans.Either as Either
-import           Control.Monad.Trans.Reader (ReaderT)
-import qualified Control.Monad.Trans.Reader as Reader (asks)
-import           Control.Monad.Trans.RWS    (RWST, asks, evalRWST, get, modify,
-                                             put)
-import           Control.Monad.Trans.State  (StateT, evalStateT, runStateT)
-import qualified Control.Monad.Trans.State  as State (get, modify, put)
-import           Data.ByteString            (ByteString)
-import           Data.Either                (Either (..), isLeft, isRight)
-import           Data.Int                   (Int64)
-import           Data.List                  (find)
-import qualified Data.Map                   as M
-import           Data.Monoid                ((<>))
-import           Data.Rehtie
-import           Data.String.Conversions
-import           Data.Text                  (Text)
-import qualified Data.Text                  as T
-import           Data.Text.Arbitrary
-import qualified Data.Text.IO               as TIO
-import           Haskell.Api.Helpers
-import           LN.Api
-import           LN.Generate
-import           LN.Sanitize
-import           LN.T
-import           LN.T.Error                 (ApplicationError (..),
-                                             ValidationError (..),
-                                             ValidationErrorCode (..))
-import           LN.Validate
-import           Prelude                    hiding (break)
-import           Rainbow
-import           System.Exit                (exitFailure)
-import           Test.QuickCheck
-import           Test.QuickCheck.Utf8
+import           Data.Monoid             ((<>))
+import           Data.String.Conversions (ConvertibleStrings, cs)
+import           Data.Text               (Text)
+import qualified Data.Text.IO            as TIO (putStrLn)
+import           LN.T.Internal.JSON      ()
+import           Rainbow                 (blue, blue, bold, chunk, cyan, fore,
+                                          green, putChunk, putChunkLn, red,
+                                          white, (&))
+import           System.Exit             (exitFailure)
 
 
 
